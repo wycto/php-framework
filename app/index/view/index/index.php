c11de751357dd522dd7cc4ee044b09ab85bb1842
wycto <294287600@qq.com>
2021-01-22 21:28:07 +0800

    使用文档

--- app/index/view/index/index.php
+++ app/index/view/index/index.php
@@ -12,7 +12,7 @@
     <h1>Welcome PHP框架</h1>
     <p>欢迎来到 php 框架</p>
     <p>使用php语言开发、采用MVC模式，简单易用，易于初学者学习和项目使用</p>
-    <p>项目地址：<a href="https://github.com/wycto/framework" target="_blank">https://github.com/wycto/framework</a></p>
+    <p>项目地址：<a href="https://github.com/wycto/php-frame" target="_blank">github代码</a></p>
     <p>作者博客：<a href="http://www.wycto.cn" target="_blank">http://www.wycto.cn</a></p>
 </div>
 </body>
