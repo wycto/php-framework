5e3c965a2582565b879afccdf92eb7926f7262d4
weiyi <weiyi@lecent.cn>
2021-01-31 10:34:31 +0800

    Session

--- frame/core/Session.php
+++ frame/core/Session.php
@@ -6,4 +6,12 @@ class Session
 
     function __construct()
     {}
+
+    function set($key,$val){
+        $_SESSION[$key] = $val;
+    }
+
+    function get($key){
+        return isset($_SESSION[$key])?$_SESSION[$key]:"";
+    }
 }
